bae9dc7f38011ae5318b9be0f49ba71baddd4df7
AlphaHosting <42865213+AlphaHosting@users.noreply.github.com>
2018-11-18 14:15:59 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,15 @@ client.on('message' , async (message) => {
     }
    });
 
+client.on("guildMemberAdd", async member => {
+  let moment2 = require('moment-duration-format'),
+      moment = require("moment"),
+      date = moment.duration(new Date() - member.user.createdAt).format("d");
+
+  if(date < 15) {
+    member.ban("Member account age is lower than 15 days.")
+  }
+});
 
 
 client.on('message', message => {
